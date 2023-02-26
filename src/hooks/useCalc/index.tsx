1c8df35f57105babd6fa6a4338ab0073b065c713
seikou <seikou615@gmail.com>
2023-02-26 11:08:07 +0000

    Change: fixed isNaN error

--- src/hooks/useCalc/index.tsx
+++ src/hooks/useCalc/index.tsx
@@ -1,24 +1,74 @@
 import React, { useEffect, useState } from "react";
 
+import { NumberError } from "utils/errorClass";
 import { Toperator } from "./types";
+import { getSelectedText } from "utils/getSelectedText";
 
-function useCalc() {
+/**
+ * A custom React hook that provides calculator functionalities.
+ *
+ * @returns {object} An object that contains the following properties:
+ *  - `compute`: A function that evaluates the expression and sets the result as the new expression.
+ *  - `del`: A function that removes the last character from the expression.
+ *  - `reset`: A function that resets the expression to an empty string.
+ *  - `setExpression`: A function that sets the expression to a given value.
+ */
+export function useCalc() {
+  /**
+   * The state variable that holds the expression for the calculator.
+   *
+   * @type {string}
+   */
   const [expression, setExpression] = useState("");
 
+  /**
+   * A function that evaluates the current expression and sets the result as the new expression.
+   *
+   * @function
+   */
   function compute() {
-    const answer = Function(`"use strict";return (${expression})`)();
+    try {
+      const answer = Function(`"use strict";return (${expression})`)();
 
-    setExpression(answer);
+      if (isNaN(Number(answer))) {
+        throw new NumberError(answer);
+      }
+
+      setExpression(answer);
+    } catch (error) {
+      setExpression("isNaN");
+    }
   }
 
+  /**
+   * A function that removes the last character from the expression.
+   *
+   * @function
+   */
   function del() {
+    if (getSelectedText() == expression) {
+      console.log(getSelectedText());
+      reset();
+    }
+
     setExpression((e) => e.slice(0, e.length - 1));
   }
 
+  /**
+   * A function that resets the expression to an empty string.
+   *
+   * @function
+   */
   function reset() {
     setExpression("");
   }
 
+  /**
+   * A function that handles key press events and updates the expression accordingly.
+   *
+   * @function
+   * @param {KeyboardEvent} e The event object for the key press event.
+   */
   function handleKeyPress(e: KeyboardEvent) {
     const key = e.key;
 
@@ -28,30 +78,38 @@ function useCalc() {
       case "+":
       case "-":
       case "*":
-      case "/": setExpression((e) => e + key);
+      case "/":
+      case ".":
+        setExpression((e) => e + key);
         break;
-        
-      case "Backspace": del();
+      case "Backspace":
+        del();
         break;
-        
-      case "Delete": reset();
+
+      case "Delete":
+      case "Clear":
+        reset();
         break;
-        
-      case '=': compute()
-      break;
+
+      case "=":
+      case "Enter":
+        compute();
+        break;
     }
   }
 
+  /**
+   * A hook that adds a key press event listener to the document to handle calculator key presses.
+   *
+   * @function
+   */
   useEffect(() => {
-    document.addEventListener("keypress", handleKeyPress);
+    document.addEventListener("keydown", handleKeyPress);
 
-    return () => document.removeEventListener("keypress", handleKeyPress);
+    return () => document.removeEventListener("keydown", handleKeyPress);
   });
 
-
-
-  return { compute, del, reset, setExpression }
-  
+  return { compute, del, reset, setExpression, expression };
 }
 
 export default useCalc;
